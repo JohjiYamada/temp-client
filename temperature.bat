7b9cd654f77ccf160ccc80f5ca0ac8fc7e7cf759
Johji Yamada <43402296+JohjiYamada@users.noreply.github.com>
2020-09-22 15:13:54 +0800

    Add files via upload

--- temperature.bat
+++ temperature.bat
@@ -1,2 +1,2 @@
 cd /d c:\work
-call winhttpjs.bat "https://temp-check.herokuapp.com/input?name=Joji%20Yamada&temp=random" -method GET
+call winhttpjs.bat "https://mc-check-nttd.herokuapp.com/input?name=Joji Yamada&mc=No&yourself=No&family=No&guest=No&office=WFH" -method GET
